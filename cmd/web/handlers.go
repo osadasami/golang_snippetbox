351ed52ccaad576441f4ec3d893cf58b6bb357aa
osadasami <osadasami@gmail.com>
2022-02-28 09:48:52 +0500

    refactor project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -32,15 +31,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Create new snippet"))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server on http://localhost:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
