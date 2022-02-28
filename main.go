7d752e1b835d24b0f207a06cdeb666e5b57618a4
osadasami <osadasami@gmail.com>
2022-02-28 09:21:25 +0500

    use http.Error shortcut

--- main.go
+++ main.go
@@ -21,8 +21,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create new snippet"))
