b5f5f1416ae179dc7374248065ec83b85f023b7f
osadasami <osadasami@gmail.com>
2022-03-01 13:21:44 +0500

    can create dummy snippet through http request

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,5 +46,16 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create new snippet"))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serveError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
